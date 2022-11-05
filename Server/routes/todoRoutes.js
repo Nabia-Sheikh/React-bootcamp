68450ed9f06f03c7174278fcca45e3af5604d63a
Nabia-Sheikh <78126275+Nabia-Sheikh@users.noreply.github.com>
2022-11-05 20:48:43 +0500

    Route error resolved

--- Server/routes/todoRoutes.js
+++ Server/routes/todoRoutes.js
@@ -11,7 +11,7 @@ const route = express.Router();
 route.post("/addTodo", addTodo)
 route.get("/getTodo", getTodo)
 route.get("/toggleTodo/:id", toggleTodo)
-route.get("/updateTodo/:id", updateTodo)
-route.get("/deleteTodo/:id", DeleteTodo)
+route.put("/updateTodo/:id", updateTodo)
+route.delete("/deleteTodo/:id", DeleteTodo)
 
 export default route;
