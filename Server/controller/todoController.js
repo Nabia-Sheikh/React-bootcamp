1311d9bcd938c6d062c642d9aa949f999f50881a
Nabia-Sheikh <78126275+Nabia-Sheikh@users.noreply.github.com>
2022-11-05 17:52:24 +0500

    Toggle todo route added

--- Server/controller/todoController.js
+++ Server/controller/todoController.js
@@ -24,3 +24,17 @@ export const getTodo = async (req, res) => {
   }
 }
 
+export const toggleTodo = async (req, res) => {
+  try {
+    const { id } = req.body
+    const todo = await Todo.findById(id)
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      id,
+      { done: !todo.done },
+      { new: true }
+    )
+    res.status(200).json(updatedTodo)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
--- Server/routes/todoRoutes.js
+++ Server/routes/todoRoutes.js
@@ -1,8 +1,9 @@
-import {addTodo,  getTodo } from "../controller/todoController.js";
+import {addTodo,  getTodo, toggleTodo } from "../controller/todoController.js";
 import express from "express";
 const route = express.Router();
 
 route.post("/addTodo", addTodo)
 route.get("/getTodo", getTodo)
+route.put("/toggleTodo", toggleTodo)
 
 export default route;
