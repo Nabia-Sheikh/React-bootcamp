a716aef9dcbd1885173ef4ef6aa853ac3d097b41
Nabia-Sheikh <78126275+Nabia-Sheikh@users.noreply.github.com>
2022-11-05 10:31:55 +0500

    Header is added

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "src/components/Header"
+import TodoForm from "./components/TodoForm"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <TodoForm />
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/components/TodoForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const TodoForm = () => {
+
+  const onFormSubmit = (e) => { 
+
+  }
+  const onInputChange = (e) => { 
+
+  }
+  return (
+    <>
+      <form className="form" onSubmit={onFormSubmit}>
+        <input
+          type="text"
+          placeholder="Add a todo"
+          className="input"
+          onchange={onInputChange}
+        />
+      </form>
+    </>
+  )
+}
+
+export default TodoForm
