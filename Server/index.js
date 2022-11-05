81e9d65d1de519df01ae27d27f4a4d2a952cd236
Nabia-Sheikh <78126275+Nabia-Sheikh@users.noreply.github.com>
2022-11-05 11:56:54 +0500

    Database connected

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,9 @@
 import express from "express"
-
+import connectDB from "./database/db.js"
 const app = express()
 
 const PORT = 8080
-
+connectDB()
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
