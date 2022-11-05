b730a255bfceaa26b836f9081021cd74345b93fc
Nabia-Sheikh <78126275+Nabia-Sheikh@users.noreply.github.com>
2022-11-05 20:47:09 +0500

    Update and delete route added

--- Server/controller/todoController.js
+++ Server/controller/todoController.js
@@ -27,15 +27,37 @@ export const getTodo = async (req, res) => {
 export const toggleTodo = async (req, res) => {
   try {
     const { id }  = req.params;
-    console.log(id);
+    // console.log(id);
     const todo = await Todo.findById(id);
-    console.log(todo);
+    // console.log(todo);
     const updatedTodo = await Todo.findByIdAndUpdate(
       id,
       { done: !todo.done },
+      { new: true }
     )
     res.status(200).json(updatedTodo)
   } catch (error) {
     res.status(404).json({ message: error.message })
+  }
+}
+
+export const updateTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { data } = req.body;
+    const updatedTodo = await Todo.findByIdAndUpdate(id, { data }, { new: true });
+    res.status(200).json(updatedTodo);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export const DeleteTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Todo.findByIdAndRemove(id);
+    res.status(200).json({ message: "Todo deleted successfully" });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
   }
 }
--- Server/routes/todoRoutes.js
+++ Server/routes/todoRoutes.js
@@ -1,9 +1,17 @@
-import {addTodo,  getTodo, toggleTodo } from "../controller/todoController.js";
+import {
+  addTodo,
+  getTodo,
+  toggleTodo,
+  updateTodo,
+  DeleteTodo,
+} from "../controller/todoController.js"
 import express from "express";
 const route = express.Router();
 
 route.post("/addTodo", addTodo)
 route.get("/getTodo", getTodo)
 route.get("/toggleTodo/:id", toggleTodo)
+route.get("/updateTodo/:id", updateTodo)
+route.get("/deleteTodo/:id", DeleteTodo)
 
 export default route;
